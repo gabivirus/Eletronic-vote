49a59e51c53ffd3e8a9da56bee55fcaf9ee052bf
Gabriel Ribeiro <118030785+gabivirus@users.noreply.github.com>
2023-04-20 22:41:26 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,16 @@
+var numInput = document.getElementById("number");
+var voteRow = [5, 3, 2];
+
+function digita(num)
+{
+    numInput.value = num;
+}
+
+window.onload = function()
+{   
+    for(var c = 0; c >= 2; c++)
+    {
+
+    }
+}
+
--- dados.php
+++ dados.php
@@ -8,4 +8,12 @@
         die("Connection failed: " . mysqli_connect_error());
     }
     echo "Connected successfully";
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+        echo "<h4 name='title' class='titulo'>title</h4>";
+        echo "<h3 name='name' class='nome'>name</h3>";
+        echo "<img name='img' src=' alt='>";
+        echo "<h3 name='number' class='numero'>number</h3>";
+        echo "<h5 name='party' class='partido'>party</h5>";
+    }
 ?>
